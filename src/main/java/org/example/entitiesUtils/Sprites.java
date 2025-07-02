63c640a5e77b45d9427f632651edbb9223694fa4
Igor Work <keaerta@gmail.com>
2025-07-02 14:18:54 +0500

    it's finished

--- src/main/java/org/example/entitiesUtils/Sprites.java
+++ src/main/java/org/example/entitiesUtils/Sprites.java
@@ -5,20 +5,13 @@ import org.example.entities.Entity;
 public class Sprites {
 
     public static String selectSprite(Entity entity) {
-        switch (entity.getClass().getSimpleName()) {
-            case "Grass" :
-                return "\uD83C\uDF3F";
-            case "Rock" :
-                return "⛰";
-            case "Tree" :
-                return "\uD83C\uDF33";
-            case "Herbivore" :
-                return "\uD83E\uDD8C";
-            case "Predator" :
-                return "\uD83D\uDC05";
-
-            default:
-                return "Unknown entity";
-        }
+        return switch (entity.getClass().getSimpleName()) {
+            case "Grass" -> "🌿";
+            case "Rock" -> "⛰";
+            case "Tree" -> "🌳";
+            case "Herbivore" -> "🦌";
+            case "Predator" -> "🐅";
+            default -> "Unknown entity";
+        };
     }
 }
